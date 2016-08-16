1729ab6eed270af3cf6894c08932c7c0224cea85
++Lβ <pluspluslb@users.noreply.github.com>
2016-08-16 08:23:37 -0500

    specify udp in run script

--- shell.sh
+++ shell.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd)/baseq3:/home/q3server/.q3a/baseq3/ -p 27960:27960 -t -i q3a-server /bin/bash
+docker run -v $(pwd)/baseq3:/home/q3server/.q3a/baseq3/ -p 27960:27960/udp -t -i q3a-server /bin/bash
